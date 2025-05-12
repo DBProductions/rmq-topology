264c9c6fb319d45772a06c5356b73ddc7a41d89e
DBCodes <dennis@dbproductions.de>
2025-05-13 00:37:53 +0200

    add import of rmq definitions

--- cypress/e2e/ui.cy.js
+++ cypress/e2e/ui.cy.js
@@ -13,7 +13,7 @@ describe('UI Component', () => {
     cy.window().its('timer.running').should('equal', false)
   })
 
-  it('Export definition', () => {
+  it('Export and copy definition', () => {
     cy.get('#export').click()
     cy.get('#imexportPanel').should('be.visible')
     cy.get('#ImExport').should(
@@ -25,9 +25,35 @@ describe('UI Component', () => {
     cy.assertValueCopiedToClipboard(
       '{"description":"","producers":[],"consumers":[],"exchanges":[],"queues":[],"bindings":[]}'
     )
+  })
+
+  it('Import definition', () => {
+    cy.get('#export').click()
+    cy.get('#imexportPanel').should('be.visible')
+    cy.get('#ImExport').type(
+      '{{}"description":"","producers":[],"consumers":[],"exchanges":[],"queues":[],"bindings":[]}'
+    )
+    cy.get('#importBtn').click()
+    cy.window().its('scene.actors.length').should('equal', 0)
+
+    cy.get('#export').click()
+    cy.get('#imexportPanel').should('be.visible')
     cy.get('#ImExport').clear()
-    cy.get('#ImExport').focus()
-    cy.get('#copyBtn').click()
-    cy.assertValueCopiedToClipboard('')
+    cy.get('#ImExport').type(
+      '{{}"description":"","producers":[],"consumers":[],"exchanges":[{{}"x": 400, "y": 150, "name": "Exchange", "type": "direct"}],"queues":[],"bindings":[]}'
+    )
+    cy.get('#importBtn').click()
+    cy.window().its('scene.actors.length').should('equal', 1)
+  })
+
+  it('Import RMQ definition', () => {
+    cy.get('#export').click()
+    cy.get('#imexportPanel').should('be.visible')
+    cy.get('#ImExport').clear()
+    cy.get('#ImExport').type(
+      '{{}"rabbit_version":"4.0.4","rabbitmq_version":"4.0.4","product_name":"RabbitMQ","product_version":"4.0.4","users":[],"vhosts":[],"permissions":[],"topic_permissions":[],"parameters":[],"global_parameters":[],"policies":[],"queues":[{{}"name":"system-events","vhost":"vhost","durable":true,"auto_delete":false,"arguments":{{}"x-queue-type":"quorum"}}],"exchanges":[{{}"name":"events","vhost":"vhost","type":"topic","durable":true,"auto_delete":false,"internal":false,"arguments":{{}}}],"bindings":[{{}"source":"events","vhost":"vhost","destination":"system-events","destination_type":"queue","routing_key":"#"}]}'
+    )
+    cy.get('#importRmqBtn').click()
+    cy.window().its('scene.actors.length').should('equal', 3)
   })
 })
