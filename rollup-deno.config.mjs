ecc0155931149fe26829e6164e313ea389b8fcb9
DBCodes <dennis@dbproductions.de>
2025-05-16 01:00:08 +0200

    add a rollup config for deno without jsdoc

---
+++ rollup-deno.config.mjs
@@ -0,0 +1,10 @@
+export default {
+  input: 'src/main.js',
+  output: {
+    sourcemap: true,
+    format: 'iife',
+    name: 'app',
+    file: 'public/scripts/script.min.js'
+  },
+  plugins: []
+}
